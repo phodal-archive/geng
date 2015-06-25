e29128aae501b4cb4ad4fd7469bb4f7bca2633e4
Fengda HUANG <h@phodal.com>
2015-06-25 08:49:45 +0800

    Generator scafflod

---
+++ specs/geng-spec.js
@@ -0,0 +1,15 @@
+describe("Main", function () {
+	var _geng;
+	beforeEach(function () {
+		_geng = new Geng();
+	});
+
+	it("should correctly parse time", function () {
+
+	});
+
+	it("should correctly convert time", function () {
+
+	});
+});
+
---
+++ src/main.js
@@ -0,0 +1,7 @@
+var Geng = function () {
+
+};
+
+Geng.version = Geng.VERSION = '0.0.0';
+
+root.Geng = Geng;
