1989374df698b444b3069e7dae2e666efb92bb36
Fengda HUANG <h@phodal.com>
2015-06-25 09:15:03 +0800

    add basic function

--- specs/geng-spec.js
+++ specs/geng-spec.js
@@ -1,15 +1,12 @@
-describe("Main", function () {
+describe('Main', function () {
 	var _geng;
 	beforeEach(function () {
 		_geng = new Geng();
 	});
 
-	it("should correctly parse time", function () {
-
-	});
-
-	it("should correctly convert time", function () {
-
+	it('should correctly convert time', function () {
+		var result = Geng.parser('子时').to('在古代是').convert();
+		expect(result).toBe('子时在古代是')
 	});
 });
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ var Geng = function () {
 
 };
 
+Geng.parser = function (time) {
+	this.time = time;
+	return this;
+};
+
+Geng.to = function (type) {
+	this.type = type;
+	return this;
+};
+
+Geng.convert = function () {
+	return this.time + this.type;
+};
+
 Geng.version = Geng.VERSION = '0.0.0';
 
 root.Geng = Geng;
