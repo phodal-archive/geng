f2ccb02e887ae901b6ffb2bf867b6accd638302e
Fengda HUANG <h@phodal.com>
2015-06-25 09:24:28 +0800

    remove dist from .gitignore & fix test

---
+++ dist/geng.js
@@ -0,0 +1,27 @@
+(function(root, undefined) {
+
+  "use strict";
+
+var Geng = function () {
+
+};
+
+Geng.parser = function (time) {
+	this.time = time;
+	return this;
+};
+
+Geng.to = function (type) {
+	this.type = type;
+	return this;
+};
+
+Geng.convert = function () {
+	return this.time + this.type;
+};
+
+Geng.version = Geng.VERSION = '0.0.0';
+
+root.Geng = Geng;
+
+}(this));
