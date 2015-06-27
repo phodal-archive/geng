ed8cb6b402273b36e07c35408d039097bf0cf076
Fengda HUANG <h@phodal.com>
2015-06-27 17:06:39 +0800

    Add a interest test

--- specs/geng-spec.js
+++ specs/geng-spec.js
@@ -6,7 +6,12 @@ describe('Main', function () {
 
 	it('should correctly convert time', function () {
 		var result = Geng.parser('子时').to('在古代是').convert();
-		expect(result).toBe('子时在古代是')
+		expect(result).toBe('子时在古代是');
+	});
+
+	it('should correctly return missing variable', function () {
+		expect(Geng.今天是).toBe(undefined);
+		expect(Geng.今天).toBe(undefined);
 	});
 });
 
