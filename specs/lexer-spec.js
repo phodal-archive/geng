a8cbe88169922fd7f979f40a88f6464cee8dd91c
Fengda HUANG <h@phodal.com>
2015-06-29 09:20:12 +0800

    Add lexer

---
+++ specs/lexer-spec.js
@@ -0,0 +1,39 @@
+describe('Trie', function () {
+	var _trie;
+	beforeEach(function () {
+		_trie = new Geng.trie();
+	});
+
+	it('should correctly split words (origin)', function () {
+		var dict = [
+			"家乡",
+			"松花",
+			"松花江",
+			"那里",
+			"四季",
+			"四季迷人",
+			"迷人",
+			"花香",
+			"hello",
+			"kitty",
+			"fine"
+		];
+		var words = "hello, kitty!我的家乡在松花江边上，那里有四季迷人的花香。fine~";
+
+		_trie.init(dict);
+		var result = [ 'hello', 'kitty', '家乡', '松花', '松花江', '那里', '四季', '四季迷人', '迷人', '花香', 'fine' ];
+		expect(_trie.splitWords(words)).toEqual(result);
+	});
+
+	it('should correctly split time words', function () {
+		var dict = [
+			"子时",
+			"古代"
+		];
+		var words = "子时在古代是";
+
+		_trie.init(dict);
+		var result = ['子时','古代'];
+		expect(_trie.splitWords(words)).toEqual(result);
+	});
+});
